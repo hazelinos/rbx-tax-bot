283a55644e45cda4f8fea01e27178c770aa6c4b5
hazelinos <feezet1@gmail.com>
2026-02-01 23:22:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {
 } = require('discord.js');
 
 const fs = require('fs');
+const path = require('path');
 
 const token = process.env.TOKEN;
 const clientId = process.env.CLIENT_ID;
@@ -19,9 +20,10 @@ client.commands = new Collection();
 
 const commands = [];
 
-const files = fs.readdirSync('./commands').filter(f => f.endsWith('.js'));
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'));
 
-for (const file of files) {
+for (const file of commandFiles) {
   const cmd = require(`./commands/${file}`);
 
   client.commands.set(cmd.data.name, cmd);
@@ -31,12 +33,16 @@ for (const file of files) {
 const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
-  await rest.put(Routes.applicationCommands(clientId), { body: commands });
-  console.log('✅ Slash registered');
+  await rest.put(
+    Routes.applicationCommands(clientId),
+    { body: commands }
+  );
+
+  console.log('✅ Slash Commands Registered:', commandFiles);
 })();
 
 client.once('clientReady', () => {
-  console.log(`✅ Bot online: ${client.user.tag}`);
+  console.log(`✅ Bot Online: ${client.user.tag}`);
 });
 
 client.on('interactionCreate', async interaction => {
@@ -45,7 +51,12 @@ client.on('interactionCreate', async interaction => {
   const cmd = client.commands.get(interaction.commandName);
   if (!cmd) return;
 
-  cmd.execute(interaction);
+  try {
+    await cmd.execute(interaction);
+  } catch (err) {
+    console.error(err);
+    interaction.reply({ content: 'Error ❌', ephemeral: true });
+  }
 });
 
 client.login(token);

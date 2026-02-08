ac85e21584111349edff9543c795b6d1974c887c
hazelinos <feezet1@gmail.com>
2026-02-08 15:54:25 +0700

    Update payment.js

--- events/payment.js
+++ events/payment.js
@@ -1,9 +1,4 @@
-const {
-  EmbedBuilder,
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle
-} = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
   name: "messageCreate",
@@ -13,36 +8,19 @@ module.exports = {
 
     const msg = message.content.toLowerCase();
 
-    // ===== TRIGGERS =====
-    const triggers = ["qr", "bayar", "qris", "payment", "pay"];
+    const triggers = ["qr", "bayar", "qris", "payment", "pay", "transfer" , "tf", "norek", "rekening"];
 
     if (!triggers.some(t => msg.includes(t))) return;
 
-    // ===== LINK QR =====
-    const qrLink = "https://cdn.discordapp.com/attachments/1443358605188534273/1469974670425198612/qr_ID1025454859550_23.12.25_176646784_1766467845652.jpg";
-
-    // ===== EMBED =====
     const embed = new EmbedBuilder()
-      .setTitle("💸 Pembayaran Nice Blox")
+      .setTitle("💳 Metode Pembayaran")
       .setDescription(
-        "Silakan scan QR di bawah atau klik tombol link untuk melakukan pembayaran.\n\n" +
-        "Setelah transfer, kirim bukti ke admin ya ✅"
+        "Silakan scan QR di bawah untuk melakukan pembayaran.\n\n" +
+        "Setelah transfer, mohon kirimkan bukti transfer"
       )
-      .setImage(qrLink)
-      .setColor("Green");
+      .setImage("https://cdn.discordapp.com/attachments/1443358605188534273/1469974670425198612/qr_ID1025454859550_23.12.25_176646784_1766467845652.jpg")
+      .setColor("Blue");
 
-    // ===== BUTTON LINK =====
-    const row = new ActionRowBuilder().addComponents(
-      new ButtonBuilder()
-        .setLabel("Bayar via Link")
-        .setStyle(ButtonStyle.Link)
-        .setURL(qrLink)
-    );
-
-    // ===== REPLY =====
-    await message.reply({
-      embeds: [embed],
-      components: [row]
-    });
+    await message.reply({ embeds: [embed] });
   }
 };

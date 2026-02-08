69a36a8076ef86282c953dbce1c478810541517e
hazelinos <feezet1@gmail.com>
2026-02-08 15:22:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -219,6 +219,19 @@ client.on('messageDelete', msg => {
 });
 
 
+/* ================ AUTO REPLY ============= */
+
+const fs = require("fs");
+const path = require("path");
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  client.on(event.name, (...args) => event.execute(...args));
+}
+
 /* ================= LOGIN ================= */
 
 client.login(token);

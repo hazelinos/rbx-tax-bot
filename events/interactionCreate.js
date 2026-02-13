5fe082909d9f221ee8d2bbeb632ec9230213955c
hazelinos <feezet1@gmail.com>
2026-02-13 12:50:50 +0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -106,37 +106,29 @@ Jika beli 100 Robux → 100 × 75 = 7.500 rupiah.`
     // ===== ROLE INFO =====
     if (interaction.customId === "roles") {
   const embed = new EmbedBuilder()
-    .setTitle("🎭 Role Information")
+    .setTitle("# Role Information #")
     .setDescription(
-`**💎 High Spender (100K Robux)**
 <@&1469353777118314688>  
 Diberikan kepada client yang telah melakukan pembelian hingga 100.000 Robux.
 
-**💰 Elite Spender (50K Robux)**
 <@&1448729340513747058>  
 Diberikan kepada client yang telah melakukan pembelian hingga 50.000 Robux.
 
-**🔥 Premium Spender (25K Robux)**
 <@&1448729350961631294>  
 Diberikan kepada client yang telah melakukan pembelian hingga 25.000 Robux.
 
-**⭐ Supporter (10K Robux)**
 <@&1448729347404988526>  
 Diberikan kepada client yang telah melakukan pembelian hingga 10.000 Robux.
 
-**🛒 Customer**
 <@&1441620125739520052>  
 Role untuk member yang telah melakukan transaksi.
 
-**👤 Member**
 <@&1449581791072751676>  
 Role dasar setelah bergabung di server.
 
-**🔔 Robux Notifications**
 <@&1450341943799316532>  
-Untuk menerima notifikasi update Robux.
+Untuk menerima notifikasi stok Robux.
 
-**🎉 Giveaway Notifications**
 <@&1450344322229862502>  
 Untuk menerima notifikasi giveaway.`)
     .setColor("#5865F2");
